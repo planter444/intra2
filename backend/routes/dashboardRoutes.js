459284a56cfd83b42d006d12c340482d49b89058
planter444 <imhuabi@gmail.com>
2026-08-14 00:41:45 +0300

    Build

--- backend/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getDashboardSummary } = require('../controllers/dashboardController');
-const { authenticate } = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.get('/summary', authenticate, getDashboardSummary);
-
-module.exports = router;
--- frontend/src/services/dashboardService.js
+++
@@ -1,6 +0,0 @@
-import api from './api';
-
-export const fetchDashboardSummary = async () => {
-  const { data } = await api.get('/dashboard/summary');
-  return data.summary;
-};
