e71aa36e8189038d7d4b34519815ec47b01324e4
planter444 <imhuabi@gmail.com>
2026-09-23 03:11:15 +0300

    Goshaii

--- backend/models/travelModel.js
+++ backend/models/travelModel.js
@@ -199,7 +199,7 @@ const createTravelRequest = async ({ userId, travelType, startDate, endDate, ori
               reference_number,
               status
             )
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19)
               RETURNING id
             `,
             [
